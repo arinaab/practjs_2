d75f0109549959a02f51c3e3c5965374b10d5b2f
Arina <arinameder123@gmail.com>
2022-02-09 12:49:23 +0700

    add module checkTextInputs

--- src/js/modules/checkTextInputs.js
+++ src/js/modules/checkTextInputs.js
@@ -1,4 +1,17 @@
 const checkTextInputs = (selector) => {
+    const txtInputs = document.querySelectorAll(selector);
+
+    txtInputs.forEach(item => {
+        let value = item.value;
+        item.addEventListener('keypress', function(e) {
+            if (e.key.match(/[^а-яё 0-9]/ig)) {
+                e.preventDefault();
+                item.value = value;
+            } else {
+                value = item.value;
+            }
+        });
+    });
 };
 
 export default checkTextInputs;
