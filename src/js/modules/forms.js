537c3aabe53b14010afa339ea1f738e8579e6ee7
Arina <arinameder123@gmail.com>
2022-02-10 13:26:23 +0700

    add db

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,3 +1,5 @@
+import {postData} from '../services/requests';
+
 const forms = () => {
     const form = document.querySelectorAll('form'),
           inputs = document.querySelectorAll('input'), //для очистки инпутов
@@ -44,15 +46,6 @@ const forms = () => {
             item.previousElementSibling.textContent = name;
         });
     });
-
-    const postData = async (url, data) => {
-        const res = await fetch(url, {
-            method: 'POST',
-            body: data
-        });
-
-        return await res.text();
-    };
 
     form.forEach(item => {
         item.addEventListener('submit', (e) => {
--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,19 +1,37 @@
-const showMoreStyles = (triggerSelector, contentSelector) => {
-    const trigger = document.querySelector(triggerSelector),
-          content = document.querySelectorAll(contentSelector);
+import { getResourse} from "../services/requests";
 
-    content.forEach(item => {
-        item.classList.add('animate__animated', 'animate__fadeIn');
-    });
+const showMoreStyles = (triggerSelector, wrapperSelector) => {
+    const trigger = document.querySelector(triggerSelector);
 
-    trigger.addEventListener('click', () => {
-        trigger.remove();
+    trigger.addEventListener('click', function() {
+        this.remove();
 
-        content.forEach(item => {
-            item.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-            item.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+        getResourse('http://localhost:3000/styles')
+        .then(res => createCards(res))
+        .catch( () => {
+            const er = document.createElement('div');
+            er.classList.add('status', 'status_er');
+            er.textContent = 'Что-то пошло не так... Сервер не отвечает';
+            document.querySelector(wrapperSelector).append(er);
         });
     });
+    
+    function createCards(response) {
+        response.forEach(({src, title, link}) => {
+
+            const card = document.createElement('div');
+            card.classList.add('animate__animated', 'animate__fadeIn', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+            card.innerHTML = `
+                <div class=styles-block>
+                    <img src=${src} alt="styles">
+                    <h4>${title}</h4>
+                    <a href="${link}">Подробнее</a>
+                </div>
+            `;
+
+            document.querySelector(wrapperSelector).append(card);
+        });
+    }
 };
 
 export default showMoreStyles;
